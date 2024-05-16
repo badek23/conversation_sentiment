314fc60141dc7467523141cda15d3e9103c2f9f6
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2024-05-17 00:15:47 +0200

    feature placeholders

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from data_preprocessing import analyze_chat_data
 from data_preprocessing import split_messages_into_users
 import data_exploration as de
+import time
 
 def column_content(user_messages, user_name):
     st.subheader(user_name)
@@ -29,6 +30,37 @@ def data_analysis(chat_dataframe, user1_messages, user2_messages):
     st.plotly_chart(de.messages_per_hour(chat_dataframe))
     st.plotly_chart(de.messages_per_day_of_week(chat_dataframe))
 
+def sentiment_analysis(chat_dataframe):
+    st.header("Sentiment Analysis")
+    #button to analyze sentiment
+    st.write("Analyze the sentiment of the chat messages.")
+
+    st.write("Select the user to analyze:")
+    sent_user = st.selectbox("User", chat_dataframe['sender'].unique(), key="su")
+
+    st.write("Select the type of sentiment analysis:")
+    analysis_type = st.selectbox("Analysis type", ["Sentiment", "Emotion"])
+
+    if st.button("Analyze"):
+        with st.spinner('Looking at the emotions...'):
+            time.sleep(1)
+            st.write("Coming soon!")
+
+def generative_ai(chat_dataframe):
+    st.header("Generative AI")
+    st.write("Generate your next message using AI.")
+
+    st.write("Select the user to generate a message for:")
+    reply_user = st.selectbox("User", chat_dataframe['sender'].unique(), key="ru")
+
+    st.write("Select the sentiment of the message:")
+    sentiment = st.selectbox("Sentiment", ["Positive", "Neutral", "Negative"])
+
+    if st.button("Generate"):
+        with st.spinner('Generating message...'):
+            time.sleep(1)
+            st.write("Coming soon!")
+
 
 # Main function to create the Streamlit app
 def main():
@@ -37,7 +69,7 @@ def main():
     st.write("Export your WhatsApp chat history and analyze it with this app! Choose to not attach media and upload the ZIP file.")
 
     # File uploader widget
-    uploaded_file = st.file_uploader("Upload CSV file", type=["zip"])
+    uploaded_file = st.file_uploader("Upload ZIP file", type=["zip"])
 
      # Provide link to WhatsApp documentation
     if uploaded_file is None:
@@ -49,10 +81,16 @@ def main():
         texts_dataframe = analyze_chat_data(uploaded_file)
         user1_messages, user2_messages = split_messages_into_users(texts_dataframe)
 
-        tab1, tab2, tab3 = st.tabs(["Data Analysis", "Sentiment Analysis", "Generative AI"])
+        tab1, tab2, tab3 = st.tabs(["Who writes the most?", "Who is angrier?", "What should I write next?"])
 
         with tab1:
             data_analysis(texts_dataframe, user1_messages, user2_messages)
+        
+        with tab2:
+            sentiment_analysis(texts_dataframe)
+
+        with tab3:
+            generative_ai(texts_dataframe)
 
 # Run the app
 if __name__ == "__main__":
